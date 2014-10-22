d3e3fee6faed0d7bc6eb2bdf56f3762bda81ffea
cliff.maury <cliff.maury@gmail.com>
2014-10-22 15:25:35 +0200

    add @Log and post processor

---
+++ src/main/java/fr/valtech/log/LoggerPostProcessor.java
@@ -0,0 +1,39 @@
+package fr.valtech.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by cliff.maury on 22/10/2014.
+ */
+@Component
+public class LoggerPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(final Object bean, String beanName) throws BeansException {
+
+        ReflectionUtils.doWithFields(bean.getClass(),new ReflectionUtils.FieldCallback() {
+            @Override
+            public void doWith(Field field) throws IllegalArgumentException, IllegalAccessException {
+                ReflectionUtils.makeAccessible(field);
+
+                if(field.isAnnotationPresent(Log.class)) {
+                    Logger logger = LoggerFactory.getLogger(bean.getClass());
+                    field.set(bean,logger);
+                }
+            }
+        });
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+}
