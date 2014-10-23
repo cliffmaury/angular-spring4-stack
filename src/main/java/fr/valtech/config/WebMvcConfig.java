a7a28e930347ae1ba7fd6ae366031d544439c4e1
cliff.maury <cliff.maury@gmail.com>
2014-10-23 18:43:09 +0200

    use alternative method to test REST controllers : with @WebAppConfiguration and @Inject for WebApplicationContext and injected bean - create mock instances in TestConfig - use SpringJunit4Runner

--- src/main/java/fr/valtech/config/WebMvcConfig.java
+++ src/main/java/fr/valtech/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package fr.valtech.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @EnableWebMvc
 @Configuration
+@ComponentScan(basePackageClasses = fr.valtech.app.rest.__Package.class)
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
 
--- src/test/java/fr/valtech/app/rest/users/UserControllerWebContextBasedTest.java
+++ src/test/java/fr/valtech/app/rest/users/UserControllerWebContextBasedTest.java
@@ -2,14 +2,20 @@ package fr.valtech.app.rest.users;
 
 import fr.valtech.app.rest.users.fixture.RestDataFixture;
 import fr.valtech.app.service.UserService;
+import fr.valtech.config.WebMvcConfig;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.Mockito;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.inject.Inject;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.*;
@@ -20,29 +26,36 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 /**
  * Created by cliff.maury on 23/10/2014.
  */
-@RunWith(MockitoJUnitRunner.class)
-public class UserControllerTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {TestConfig.class, WebMvcConfig.class,})
+@WebAppConfiguration
+public class UserControllerWebContextBasedTest {
 
     // see tutorial
     // http://www.petrikainulainen.net/programming/spring-framework/unit-testing-of-spring-mvc-controllers-configuration#appcontext-config
 
     private MockMvc mockMvc;
 
-    @Mock
+    @Inject
     private UserService userServiceMock;
 
-    private UserController userController;
+    @Inject
+    private WebApplicationContext webApplicationContext;
 
     @Before
     public void setup() {
-        userController = new UserController(userServiceMock);
-        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+        //We have to reset our mock between tests because the mock objects
+        //are managed by the Spring container. If we would not reset them,
+        //stubbing and verified behavior would "leak" from one test to another.
+        Mockito.reset(userServiceMock);
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
     @Test
     public void findAllUsers_should_return_3_users() throws Exception {
 
         when(userServiceMock.findAllUsers()).thenReturn(RestDataFixture.findAllUsers());
+
         mockMvc.perform(get("/users").accept(MediaType.APPLICATION_JSON)).
                 andExpect(status().isOk()).
                 andExpect(content().contentType(TestUtil.APPLICATION_JSON_UTF8)).
