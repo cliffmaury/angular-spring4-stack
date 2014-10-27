c53c73867528b614fc843442d6b20ad1c53b3420
cliff.maury <cliff.maury@gmail.com>
2014-10-27 15:02:53 +0100

    add test class for UserService

---
+++ src/test/java/fr/valtech/angularspring/app/service/UserServiceTest.java
@@ -0,0 +1,27 @@
+package fr.valtech.angularspring.app.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by cliff.maury on 27/10/2014.
+ */
+public class UserServiceTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void testCreateUser() {
+
+    }
+
+    @Test
+    public void testFindAllUsers() {
+
+    }
+
+
+}
