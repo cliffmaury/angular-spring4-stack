eeb7cf5c34e2aea83323a3cf9a77eb999852b250
cliff.maury <cliff.maury@gmail.com>
2014-10-30 12:25:00 +0100

    log active spring profiles on application init

--- src/main/java/fr/valtech/angularspring/config/ApplicationConfig.java
+++ src/main/java/fr/valtech/angularspring/config/ApplicationConfig.java
@@ -1,7 +1,14 @@
 package fr.valtech.angularspring.config;
 
+import fr.valtech.angularspring.log.Log;
+import org.slf4j.Logger;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import java.util.Arrays;
 
 /**
  * Created by cliff.maury on 30/10/2014.
@@ -17,4 +24,27 @@ import org.springframework.context.annotation.Import;
         AppConfig.class
 })
 public class ApplicationConfig {
+
+    @Log
+    private static Logger logger;
+
+    @Inject
+    private Environment environment;
+
+    /**
+     * Application initialization
+     * <p>
+     * Spring profiles are set by -Dspring.profiles.active=PROFILE
+     */
+    @PostConstruct
+    public void initApp() {
+        logger.debug("Looking for Spring profiles");
+
+        if (environment.getActiveProfiles().length == 0) {
+            logger.info("No Spring profile set, running with default configuration.");
+        } else {
+            logger.info("Detected Spring profiles : {}", Arrays.toString(environment.getActiveProfiles()));
+        }
+    }
+
 }
