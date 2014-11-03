3a7a4132c5229f5144de2332ced91623a2c57aff
cliff.maury <cliff.maury@gmail.com>
2014-11-03 11:21:16 +0100

    implement finadAll with userRepository

--- src/main/java/fr/valtech/angularspring/app/service/impl/UserServiceImpl.java
+++ src/main/java/fr/valtech/angularspring/app/service/impl/UserServiceImpl.java
@@ -1,12 +1,13 @@
 package fr.valtech.angularspring.app.service.impl;
 
 import fr.valtech.angularspring.app.domain.User;
+import fr.valtech.angularspring.app.repository.UserRepository;
 import fr.valtech.angularspring.app.service.UserService;
 import fr.valtech.angularspring.log.Log;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import javax.inject.Inject;
 import java.util.List;
 
 /**
@@ -18,11 +19,14 @@ public class UserServiceImpl implements UserService {
     @Log
     private Logger logger;
 
+    @Inject
+    private UserRepository userRepository;
+
     public void createUser(String name, String lastName) {
         logger.warn("create User");
     }
 
     public List<User> findAllUsers() {
-        throw new NotImplementedException();
+        return userRepository.findAll();
     }
 }
