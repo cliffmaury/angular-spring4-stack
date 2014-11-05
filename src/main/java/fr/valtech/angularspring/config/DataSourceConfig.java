4a88cea88f26ce3a241dc584801792345a4f86da
cliff.maury <cliff.maury@gmail.com>
2014-11-05 15:05:23 +0100

    Add HikariCP with MySQL connector Configure Hikari from properties file

--- src/main/java/fr/valtech/angularspring/config/DataSourceConfig.java
+++ src/main/java/fr/valtech/angularspring/config/DataSourceConfig.java
@@ -1,5 +1,7 @@
 package fr.valtech.angularspring.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -25,8 +27,19 @@ import javax.sql.DataSource;
 @PropertySource("classpath:config/application-${spring.profiles.active:" + Profiles.DEFAULT + "}.properties")
 public class DataSourceConfig {
 
-    public static final String DB_DRIVER = "db.driver";
-    public static final String DB_URL = "db.url";
+    public static final String PREFIX = "db.";
+
+    public static final String DB_DRIVER = PREFIX + "driver";
+    public static final String DB_URL = PREFIX + "url";
+    public static final String DB_USERNAME = PREFIX + "user";
+    public static final String DB_PASSWORD = PREFIX + "password";
+    public static final String DATA_SOURCE_CLASS_NAME = "dataSourceClassName";
+    public static final String DATABASE_NAME = "databaseName";
+    public static final String SERVER_NAME = "serverName";
+    public static final String CACHE_PREP_STMTS = "cachePrepStmts";
+    public static final String PREP_STMT_CACHE_SIZE = "prepStmtCacheSize";
+    public static final String PREP_STMT_CACHE_SQL_LIMIT = "prepStmtCacheSqlLimit";
+    public static final String USE_SERVER_PREP_STMTS = "useServerPrepStmts";
 
     @Inject
     private Environment environment;
@@ -41,14 +54,49 @@ public class DataSourceConfig {
     @Profile(Profiles.DEV_STANDALONE)
     public DataSource dataSourceDEV() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(environment.getProperty(DB_DRIVER));
-        dataSource.setUrl(environment.getProperty(DB_URL));
+        dataSource.setDriverClassName(getValue(DB_DRIVER));
+        dataSource.setUrl(getValue(DB_URL));
         return dataSource;
     }
 
     @Bean
+    @Profile({ Profiles.STAGING, Profiles.PRODUCTION })
+    public DataSource dataSourceHIKARI() {
+        HikariConfig config = new HikariConfig();
+        config.setDataSourceClassName(getValue(DATA_SOURCE_CLASS_NAME));
+        config.setUsername(getValue(DB_USERNAME));
+        config.setPassword(getValue(DB_PASSWORD));
+
+        config.addDataSourceProperty(DATABASE_NAME, getValue(PREFIX + DATABASE_NAME));
+        config.addDataSourceProperty(SERVER_NAME, getValue(PREFIX + SERVER_NAME));
+
+        //MySQL optimizations, see https://github.com/brettwooldridge/HikariCP/wiki/MySQL-Configuration
+        config.addDataSourceProperty(CACHE_PREP_STMTS, getValue(PREFIX + CACHE_PREP_STMTS, "true"));
+        config.addDataSourceProperty(PREP_STMT_CACHE_SIZE, getValue(PREFIX + PREP_STMT_CACHE_SIZE, "250"));
+        config.addDataSourceProperty(PREP_STMT_CACHE_SQL_LIMIT, getValue(PREFIX + PREP_STMT_CACHE_SQL_LIMIT, "2048"));
+        config.addDataSourceProperty(USE_SERVER_PREP_STMTS, getValue(PREFIX + USE_SERVER_PREP_STMTS, "true"));
+
+        return new HikariDataSource(config);
+    }
+
+    @Bean
     @Profile({ Profiles.TEST, Profiles.DEV, Profiles.DEV_STANDALONE })
-    public Database database() {
+    public Database databaseDEV() {
         return Database.HSQL;
     }
+
+    @Bean
+    @Profile({ Profiles.STAGING, Profiles.PRODUCTION })
+    public Database databasePROD() {
+        return Database.MYSQL;
+    }
+
+    private String getValue(String key) {
+        return environment.getProperty(key);
+    }
+
+    private String getValue(String key, String defaultValue) {
+        return environment.getProperty(key, defaultValue);
+    }
+
 }
--- src/main/java/fr/valtech/angularspring/config/Profiles.java
+++ src/main/java/fr/valtech/angularspring/config/Profiles.java
@@ -8,7 +8,8 @@ public class Profiles {
     public static final String TEST = "test";
     public static final String DEV = "dev";
     public static final String DEV_STANDALONE = "dev-standalone";
-    public static final String DEFAULT = DEV_STANDALONE;
     public static final String STAGING = "staging";
     public static final String PRODUCTION = "production";
+
+    public static final String DEFAULT = Profiles.PRODUCTION;
 }
