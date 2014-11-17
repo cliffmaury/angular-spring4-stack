707d014d7a68cc9f8e4d1976555e4e94f5cec7bd
cliff.maury <cliff.maury@gmail.com>
2014-11-17 17:45:05 +0100

    set Karma server port to 8000 to avoid conflict when backend server is running

--- src/main/webapp/test/karma.conf.js
+++ src/main/webapp/test/karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function(config) {
     exclude: [],
 
     // web server port
-    port: 8080,
+    port: 8000,
 
     // Start these browsers, currently available:
     // - Chrome
